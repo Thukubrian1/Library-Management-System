65e2865e06afca0f83bcc55a7380619a0a727757
Thukubrian1 <thukubrianngugi1@gmail.com>
2025-05-15 16:14:00 +0300

    My library management system

---
+++ library/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO BOOK(ID, AUTHOR, BOOK_NAME, PUBLISHER)
+VALUES
+(1, 'H.R Olekulet', 'Blossoms of the Savannah', 'Macmillan'),
+(2, 'Brian Ngugi', 'Wages of sin', 'Macmillan'),
+(3, 'Jayden Mugi', 'Bush Trackers', 'Macmillan');
+
+INSERT INTO AUTHORS(ID, AUTHOR_NAME, EMAIL)
+VALUES
+(1, 'Brian Ngugi','test1@gmail.com'),
+(2, 'Ethan Muia','test2@gmail.com'),
+(3, 'Jayden Mugi', 'test3@gmail.com');
+
---
+++ library/src/test/java/com/lms/library/Controller/BookControllerTest.java
@@ -0,0 +1,28 @@
+package com.lms.library.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookControllerTest {
+
+    @Test
+    void getAllBook() {
+    }
+
+    @Test
+    void getBookById() {
+    }
+
+    @Test
+    void addBook() {
+    }
+
+    @Test
+    void updateBook() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+}
